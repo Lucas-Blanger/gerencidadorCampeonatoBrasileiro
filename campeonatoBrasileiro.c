c04f126a521a2a00f89c88bed04a7d7b5000b15d
Lucas-Blanger <blangerlucas@gmail.com>
2024-11-12 11:21:14 -0300

    feat: funções de excluir

--- campeonatoBrasileiro.c
+++ campeonatoBrasileiro.c
@@ -8,7 +8,6 @@
 #define MAX_PARTIDAS 10
 #define MAX_EVENTOS 50
 
-// Estruturas
 typedef struct {
     char nome[50];
     char estado[3];
@@ -48,7 +47,6 @@ typedef struct {
     int totalPartidas;
 } Campeonato;
 
-// Funções
 void inicializarCampeonato(Campeonato *c) {
     c->totalTimes = 0;
     c->totalPartidas = 0;
@@ -93,6 +91,18 @@ void cadastrarPartida(Campeonato *c, int time1Index, int time2Index) {
     }
 }
 
+Jogador* encontrarJogadorPorNome(Campeonato *c, int timeIndex, const char *nomeJogador) {
+    if (timeIndex >= 0 && timeIndex < c->totalTimes) {
+        Time *t = &c->times[timeIndex];
+        for (int i = 0; i < t->jogadoresAtuais; i++) {
+            if (strcmp(c->jogadores[timeIndex][i].nome, nomeJogador) == 0) {
+                return &c->jogadores[timeIndex][i];
+            }
+        }
+    }
+    return NULL;
+}
+
 void narrarEvento(Campeonato *c, int partidaIndex, int minuto, char evento[]) {
     if (partidaIndex >= 0 && partidaIndex < c->totalPartidas) {
         Partida *p = &c->partidas[partidaIndex];
@@ -101,10 +111,44 @@ void narrarEvento(Campeonato *c, int partidaIndex, int minuto, char evento[]) {
             e->minuto = minuto;
             strcpy(e->evento, evento);
             p->totalEventos++;
+            
+            // Processa o evento para atualizar estatísticas
+            if (strstr(evento, "Gol de") != NULL) {
+                char nomeJogador[50];
+                sscanf(evento, "Gol de %49[^\n]", nomeJogador);
+                Jogador *jogador = encontrarJogadorPorNome(c, (strstr(nomeJogador, "A") ? 0 : 1), nomeJogador);
+                if (jogador != NULL) {
+                    jogador->gols++;
+                    if (p->time1 == &c->times[0]) {
+                        p->time1->golsFeitos++;
+                        p->time2->golsSofridos++;
+                    } else {
+                        p->time2->golsFeitos++;
+                        p->time1->golsSofridos++;
+                    }
+                }
+            } else if (strstr(evento, "Cartão Amarelo para") != NULL) {
+                char nomeJogador[50];
+                sscanf(evento, "Cartão Amarelo para %49[^\n]", nomeJogador);
+                Jogador *jogador = encontrarJogadorPorNome(c, (strstr(nomeJogador, "A") ? 0 : 1), nomeJogador);
+                if (jogador != NULL) {
+                    (strstr(nomeJogador, "A") ? &c->times[0] : &c->times[1])->cartoesAmarelos++;
+                }
+            } else if (strstr(evento, "Cartão Vermelho para") != NULL) {
+                char nomeJogador[50];
+                sscanf(evento, "Cartão Vermelho para %49[^\n]", nomeJogador);
+                Jogador *jogador = encontrarJogadorPorNome(c, (strstr(nomeJogador, "A") ? 0 : 1), nomeJogador);
+                if (jogador != NULL) {
+                    (strstr(nomeJogador, "A") ? &c->times[0] : &c->times[1])->cartoesVermelhos++;
+                }
+            } else if (strstr(evento, "Falta de") != NULL) {
+                (strstr(evento, "Jogador_A") ? &c->times[0] : &c->times[1])->faltas++;
+            }
         }
     }
 }
 
+// Relatórios
 void imprimirRelatorioClassificacao(Campeonato *c) {
     printf("Classificação Geral:\n");
     for (int i = 0; i < c->totalTimes; i++) {
@@ -132,31 +176,149 @@ void imprimirRelatorioEstatisticas(Campeonato *c) {
     }
 }
 
-// Função principal
+void mostrarRelatorio(Campeonato *c) {
+    imprimirRelatorioClassificacao(c);
+    printf("\n");
+    imprimirRelatorioArtilharia(c);
+    printf("\n");
+    imprimirRelatorioEstatisticas(c);
+}
+
+void excluirTime(Campeonato *c, int timeIndex) {
+    if (timeIndex >= 0 && timeIndex < c->totalTimes) {
+        for (int i = timeIndex; i < c->totalTimes - 1; i++) {
+            c->times[i] = c->times[i + 1];
+            memcpy(c->jogadores[i], c->jogadores[i + 1], sizeof(Jogador) * MAX_JOGADORES);
+        }
+        c->totalTimes--;
+    }
+}
+
+void excluirJogador(Campeonato *c, int timeIndex, int jogadorIndex) {
+    if (timeIndex >= 0 && timeIndex < c->totalTimes &&
+        jogadorIndex >= 0 && jogadorIndex < c->times[timeIndex].jogadoresAtuais) {
+        for (int i = jogadorIndex; i < c->times[timeIndex].jogadoresAtuais - 1; i++) {
+            c->jogadores[timeIndex][i] = c->jogadores[timeIndex][i + 1];
+        }
+        c->times[timeIndex].jogadoresAtuais--;
+    }
+}
+
+void excluirPartida(Campeonato *c, int partidaIndex) {
+    if (partidaIndex >= 0 && partidaIndex < c->totalPartidas) {
+        for (int i = partidaIndex; i < c->totalPartidas - 1; i++) {
+            c->partidas[i] = c->partidas[i + 1];
+        }
+        c->totalPartidas--;
+    }
+}
+
+Time* buscarTime(Campeonato *c, char nome[]) {
+    for (int i = 0; i < c->totalTimes; i++) {
+        if (strcmp(c->times[i].nome, nome) == 0) {
+            return &c->times[i];
+        }
+    }
+    return NULL;
+}
+
+Jogador* buscarJogador(Campeonato *c, int timeIndex, char nome[]) {
+    if (timeIndex >= 0 && timeIndex < c->totalTimes) {
+        for (int i = 0; i < c->times[timeIndex].jogadoresAtuais; i++) {
+            if (strcmp(c->jogadores[timeIndex][i].nome, nome) == 0) {
+                return &c->jogadores[timeIndex][i];
+            }
+        }
+    }
+    return NULL;
+}
+
+Partida* buscarPartida(Campeonato *c, int time1Index, int time2Index) {
+    for (int i = 0; i < c->totalPartidas; i++) {
+        if (c->partidas[i].time1 == &c->times[time1Index] &&
+            c->partidas[i].time2 == &c->times[time2Index]) {
+            return &c->partidas[i];
+        }
+    }
+    return NULL;
+}
+
+void montarEscalacaoPadrao(Campeonato *c, int timeIndex, int numJogadores) {
+    if (timeIndex >= 0 && timeIndex < c->totalTimes && numJogadores <= c->times[timeIndex].jogadoresAtuais) {
+        printf("Escalação padrão para o time %s:\n", c->times[timeIndex].nome);
+        for (int i = 0; i < numJogadores; i++) {
+            Jogador *j = &c->jogadores[timeIndex][i];
+            printf("Nome: %s | Posição: %s | Camisa: %d\n", j->nome, j->posicao, j->numeroCamisa);
+        }
+    }
+}
+
+
 int main() {
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
     Campeonato campeonato;
     inicializarCampeonato(&campeonato);
+    
+    cadastrarTime(&campeonato, "15 de Piracicaba", "SP");
+    cadastrarTime(&campeonato, "Gaúcho", "RS");
+    cadastrarTime(&campeonato, "Ypiranga de Erechim", "RS");
+    
+    for (int i = 0; i < 11; i++) {
+        char nome[20];
+        sprintf(nome, "Jogador_A%d", i + 1);
+        cadastrarJogador(&campeonato, 0, nome, "Atacante", 20 + i, i + 1);
+    }
 
-    // Cadastrar 2 times
-    cadastrarTime(&campeonato, "Internacional", "RS");
-    cadastrarTime(&campeonato, "Flamengo", "RJ");
+    for (int i = 0; i < 11; i++) {
+        char nome[20];
+        sprintf(nome, "Jogador_B%d", i + 1);
+        cadastrarJogador(&campeonato, 1, nome, "Defensor", 22 + i, i + 1);
+    }
 
-    // Cadastrar jogadores para os times
-    cadastrarJogador(&campeonato, 0, "Gabriel", "Atacante", 25, 10);
-    cadastrarJogador(&campeonato, 0, "Igor", "Defensor", 28, 4);
-    cadastrarJogador(&campeonato, 1, "Ener", "Atacante", 22, 9);
-    cadastrarJogador(&campeonato, 1, "Carlos", "Defensor", 30, 5);
-
-    // Cadastrar duas partidas
     cadastrarPartida(&campeonato, 0, 1);
-    narrarEvento(&campeonato, 0, 10, "Gol do Gabriel");
-    narrarEvento(&campeonato, 0, 45, "Cartão amarelo para Carlos");
+    cadastrarPartida(&campeonato, 1, 0);
 
-    // Relatórios
-    imprimirRelatorioClassificacao(&campeonato);
-    imprimirRelatorioArtilharia(&campeonato);
-    imprimirRelatorioEstatisticas(&campeonato);
+    narrarEvento(&campeonato, 0, 5, "Gol de Jogador_A1");
+    narrarEvento(&campeonato, 0, 15, "Cartão Amarelo para Jogador_B3");
+    narrarEvento(&campeonato, 0, 45, "Falta de Jogador_A4 em Jogador_B5");
 
+    narrarEvento(&campeonato, 1, 10, "Gol de Jogador_B2");
+    narrarEvento(&campeonato, 1, 30, "Cartão Vermelho para Jogador_A7");
+    narrarEvento(&campeonato, 1, 60, "Substituição: Sai Jogador_A9, Entra Jogador_A10");
+
+    
+    mostrarRelatorio(&campeonato);
+
+    Time* time = buscarTime(&campeonato, "Gaúcho");
+    if (time != NULL) {
+        int timeIndex = time - campeonato.times;
+        excluirTime(&campeonato, timeIndex);
+    }
+
+    cadastrarTime(&campeonato, "Gaúcho", "RS");
+    cadastrarJogador(&campeonato, 1, "Jogador_B1", "Goleiro", 27, 1);
+
+
+    Jogador* jogador = buscarJogador(&campeonato, 0, "Jogador_A2");
+    if (jogador != NULL) {
+        int jogadorIndex = jogador - campeonato.jogadores[0];
+        excluirJogador(&campeonato, 0, jogadorIndex);
+    }
+
+    Partida* partida = buscarPartida(&campeonato, 0, 1);
+    if (partida != NULL) {
+        int partidaIndex = partida - campeonato.partidas;
+        excluirPartida(&campeonato, partidaIndex);
+    }
+
+    Jogador* jogadorB1 = buscarJogador(&campeonato, 1, "Jogador_B1");
+    if (jogadorB1 != NULL) {
+        printf("Jogador encontrado: %s - Posição: %s - Camisa: %d\n", jogadorB1->nome, jogadorB1->posicao, jogadorB1->numeroCamisa);
+    } else {
+        printf("Jogador_B1 não encontrado\n");
+    }
+
+    montarEscalacaoPadrao(&campeonato, 0, 2);
+    
 }
